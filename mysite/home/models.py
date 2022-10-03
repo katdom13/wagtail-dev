41a5cc353e0a14dffb99989c194d4944ea286980
katdom13 <katdom13@gmail.com>
2022-10-03 10:47:44 +0800

    Added bootstrap and design for homepage

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,5 +1,8 @@
+from email.policy import default
+
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.core.fields import RichTextField
 from wagtail.models import Page
 
 
@@ -15,9 +18,27 @@ class HomePage(Page):
     max_count = 1
 
     banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"], null=True, default="Subtitle Text")
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title")
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        FieldPanel("banner_image"),
+        PageChooserPanel("banner_cta")
     ]
 
 
